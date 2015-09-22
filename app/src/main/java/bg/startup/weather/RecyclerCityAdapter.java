dac2e01cafa6f25c692147519eaf3600bbd9f5f6
unknown <hristova.radostina@gmail.com>
2015-09-22 22:41:05 +0300

    RecyclerView demo with City

---
+++ app/src/main/java/bg/startup/weather/RecyclerCityAdapter.java
@@ -0,0 +1,55 @@
+package bg.startup.weather;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
+
+import java.util.ArrayList;
+
+public class RecyclerCityAdapter extends RecyclerView.Adapter<RecyclerCityAdapter.CityViewHolder> {
+
+    private Context mContext;
+    private ArrayList<City> mCities;
+
+    public RecyclerCityAdapter(Context context, ArrayList<City> listCities) {
+        mContext = context;
+        mCities = listCities;
+    }
+
+    @Override
+    public CityViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        Log.i("Weather", "RecyclerView Adapter: onCreateViewHolder;");
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.item_list_view, viewGroup, false);
+        return new CityViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CityViewHolder cityViewHolder, int i) {
+        Log.i("Weather", "RecyclerView Adapter: onBindViewHolder; position=" + i);
+
+        City city = mCities.get(i);
+        cityViewHolder.textView.setText(city.getName());
+        cityViewHolder.imageView.setImageDrawable(city.getIcon());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCities.size();
+    }
+
+    public class CityViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+        public TextView textView;
+
+        public CityViewHolder(View itemView) {
+            super(itemView);
+
+            this.imageView = (ImageView) itemView.findViewById(R.id.icon);
+            this.textView = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+}
---
+++ app/src/main/java/bg/startup/weather/RecyclerViewActivity.java
@@ -0,0 +1,37 @@
+package bg.startup.weather;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.*;
+
+import java.util.ArrayList;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerCityAdapter(this, initializeCities()));
+
+    }
+
+    private ArrayList<City> initializeCities() {
+        String[] cities = {"London", "Sofia", "New York", "Athens", "Belgrade", "Bucharest", "Skopje",
+                "Berlin", "Helsinki", "Rome", "Madrid", "Lisbon", "Riga", "Kiev"};
+        Drawable icon = getResources().getDrawable(R.mipmap.ic_launcher);
+        ArrayList<City> listCities = new ArrayList<>();
+        for (String city : cities) {
+            City newCity = new City();
+            newCity.setName(city);
+            newCity.setIcon(icon);
+            listCities.add(newCity);
+        }
+
+        return listCities;
+    }
+}
