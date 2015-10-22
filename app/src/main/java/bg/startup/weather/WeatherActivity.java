c3f75a99d2543d8e9e34fe87436124c69a142209
unknown <hristova.radostina@gmail.com>
2015-10-22 19:39:06 +0300

    Load weather in Weather Activity

--- app/src/main/java/bg/startup/weather/WeatherActivity.java
+++ app/src/main/java/bg/startup/weather/WeatherActivity.java
@@ -1,8 +1,11 @@
 package bg.startup.weather;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.*;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -11,6 +14,8 @@ public class WeatherActivity extends AppCompatActivity {
 
     private static final String API = "http://api.openweathermap.org/data/2.5/weather?q=";
     private static final String KEY = "&appid=bd82977b86bf27fb59a04b61b657fb6f";
+
+    private WeatherResponse weather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,10 @@ public class WeatherActivity extends AppCompatActivity {
                     @Override
                     protected void onPostExecute(WeatherResponse weatherResponse) {
                         super.onPostExecute(weatherResponse);
-                        loadWeather(weatherResponse);
+                        if (weatherResponse != null) {
+                            weather  = weatherResponse;
+                            loadWeather(weatherResponse);
+                        }
                     }
                 }.execute(url);
             } catch (UnsupportedEncodingException e) {
@@ -39,6 +47,65 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     private void loadWeather(WeatherResponse weatherResponse) {
+        String cityName = weatherResponse.getName();
+        setCityName(cityName);
+
+        //Icon
+
+        double temperature = weatherResponse.getInfo().getTemperature();
+        setTemperature(getTemperatureInCelsius(temperature));
+
+        setupTempButtons();
+        //temp min max
+        // weather description
 
     }
+
+    private void setCityName(String cityName) {
+        TextView textViewCity = (TextView) findViewById(R.id.text_city_name);
+        textViewCity.setText(cityName);
+    }
+
+    private void setTemperature(double temperature) {
+        TextView textViewTemp = (TextView) findViewById(R.id.text_temperature);
+        String formattedTemperature = String.valueOf(temperature) + getString(R.string.degree);
+        textViewTemp.setText(formattedTemperature);
+    }
+
+    private void setupTempButtons() {
+        final Button buttonCelsius = (Button) findViewById(R.id.button_celsius);
+        final Button buttonFahrenheit = (Button) findViewById(R.id.button_fahrenheit);
+
+        buttonCelsius.setTypeface(null, Typeface.BOLD_ITALIC);
+
+        buttonCelsius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonCelsius.setTypeface(null, Typeface.BOLD_ITALIC);
+                buttonFahrenheit.setTypeface(null, Typeface.NORMAL);
+                double temp = weather.getInfo().getTemperature();
+                setTemperature(getTemperatureInCelsius(temp));
+
+            }
+        });
+
+        buttonFahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonFahrenheit.setTypeface(null, Typeface.BOLD_ITALIC);
+                buttonCelsius.setTypeface(null, Typeface.NORMAL);
+                double temp = weather.getInfo().getTemperature();
+                setTemperature(getTemperatureInFahrenheit(temp));
+            }
+        });
+    }
+
+    private int getTemperatureInCelsius(double temperature) {
+        return (int) (temperature - 273.15d);
+    }
+
+    private int getTemperatureInFahrenheit(double temperature) {
+        return (int) ((temperature - 273.15d) * 1.8d + 32.0d);
+    }
+
 }
