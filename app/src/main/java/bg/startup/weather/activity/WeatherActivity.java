b1141da43fef7a906de2c489faa28acfaa8e1ef6
unknown <hristova.radostina@gmail.com>
2015-10-26 20:23:28 +0200

    Added temp min/max and description; Code refactor

--- app/src/main/java/bg/startup/weather/activity/WeatherActivity.java
+++ app/src/main/java/bg/startup/weather/activity/WeatherActivity.java
@@ -23,6 +23,8 @@ public class WeatherActivity extends AppCompatActivity {
     private Typeface typeface;
     private WeatherResponse weather;
 
+    private boolean isCelsius = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +46,7 @@ public class WeatherActivity extends AppCompatActivity {
 
                         if (weatherResponse != null) {
                             weather = weatherResponse;
-                            loadWeather(weatherResponse);
+                            loadWeather();
                         }
 
                     }
@@ -56,37 +58,40 @@ public class WeatherActivity extends AppCompatActivity {
         }
     }
 
-    private void loadWeather(WeatherResponse weatherResponse) {
-        String cityName = weatherResponse.getName();
-        setCityName(cityName);
+    private void loadWeather() {
 
-        setIcon(weatherResponse.getWeather().getIcon());
+        setCityName();
 
-        double temperature = weatherResponse.getInfo().getTemperature();
-        setTemperature(getTemperatureInCelsius(temperature));
+        setIcon();
+
+        setTemperature();
 
         setupTempButtons();
-        //temp min max
-        // weather description
+
+        setTempMinMax();
+
+        setDescription();
 
     }
 
-    private void setCityName(String cityName) {
+    private void setCityName() {
         TextView textViewCity = (TextView) findViewById(R.id.text_city_name);
         textViewCity.setTypeface(typeface);
-        textViewCity.setText(cityName);
+        textViewCity.setText(weather.getName());
     }
 
-    private void setIcon(String iconName) {
-        iconName = "i" + iconName;
+    private void setIcon() {
+        String iconName = "i" + weather.getWeather().getIcon();
         int iconId = getResources().getIdentifier(iconName, "drawable", getPackageName());
         ImageView imageView = (ImageView) findViewById(R.id.image_weather);
         imageView.setImageResource(iconId);
     }
 
-    private void setTemperature(double temperature) {
+    private void setTemperature() {
         TextView textViewTemp = (TextView) findViewById(R.id.text_temperature);
-        String formattedTemperature = String.valueOf(temperature) + getString(R.string.degree);
+        double temperature = weather.getInfo().getTemperature();
+        String formattedTemperature = isCelsius ? (getTemperatureInCelsius(temperature) + getString(R.string.degree))
+                : (getTemperatureInFahrenheit(temperature) + getString(R.string.degree));
         textViewTemp.setTypeface(typeface);
         textViewTemp.setText(formattedTemperature);
     }
@@ -104,23 +109,48 @@ public class WeatherActivity extends AppCompatActivity {
         buttonCelsius.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                isCelsius = true;
                 buttonCelsius.setTypeface(typeface, Typeface.BOLD);
                 buttonFahrenheit.setTypeface(typeface, Typeface.NORMAL);
-                double temp = weather.getInfo().getTemperature();
-                setTemperature(getTemperatureInCelsius(temp));
-
+                setTemperature();
+                setTempMinMax();
             }
         });
 
         buttonFahrenheit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                isCelsius = false;
                 buttonFahrenheit.setTypeface(typeface, Typeface.BOLD);
                 buttonCelsius.setTypeface(typeface, Typeface.NORMAL);
-                double temp = weather.getInfo().getTemperature();
-                setTemperature(getTemperatureInFahrenheit(temp));
+                setTemperature();
+                setTempMinMax();
             }
         });
+    }
+
+    private void setTempMinMax() {
+        TextView textMinMax = (TextView) findViewById(R.id.text_temp_min_max);
+        textMinMax.setTypeface(typeface);
+
+        WeatherResponse.WeatherInfo weatherInfo = weather.getInfo();
+
+        int formattedTempMin = isCelsius ? getTemperatureInCelsius(weatherInfo.getTempMin()) :
+                getTemperatureInFahrenheit(weatherInfo.getTempMin());
+        int formattedTempMax = isCelsius ? getTemperatureInCelsius(weatherInfo.getTempMax()) :
+                getTemperatureInFahrenheit(weatherInfo.getTempMax());
+
+        StringBuilder tempBuilder = new StringBuilder();
+        tempBuilder.append(formattedTempMin).append(getString(R.string.degree))
+                .append(" - ").append(formattedTempMax).append(getString(R.string.degree));
+        textMinMax.setText(tempBuilder.toString());
+    }
+
+    private void setDescription() {
+        TextView textDescription = (TextView) findViewById(R.id.text_weather_description);
+        textDescription.setTypeface(typeface);
+        String weatherDescription = weather.getWeather().getWeather();
+        textDescription.setText(weatherDescription);
     }
 
     private int getTemperatureInCelsius(double temperature) {
